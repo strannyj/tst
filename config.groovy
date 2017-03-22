2f8998470addbb7970bed3ec9bdc5ea63ef4c0c4
strannyj <araksik@ukr.net>
2017-03-22 16:16:04 +0200

    Initial import

---
+++ config.groovy
@@ -0,0 +1,15 @@
+// The title of this document
+title = "somethng"
+
+// The version of this document
+version = "1.0"
+
+// The path of source files directory
+//sourceDirectoryPath = "."
+
+// The path of directory to be outputted a document
+outputDirectoryPath = "docs"
+
+numbering = false
+homePage = "README.md"
+documentTocDepth = 2
